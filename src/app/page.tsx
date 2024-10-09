d12c1ac8ef071821623628289e828cbe368972aa
Shahzer Baig <shahzer.baig.sb@gmail.com>
2024-10-09 21:25:00 +0530

    Added Fetch funcionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
-export default function Home() {
-  return (
+'use client'
+import { useEffect, useState } from "react";
 
-            <div className="bg-slate-50 text-black rounded-xl flex align-middle justify-center w-52">Hello, from Main server</div>
+
+export default function Home() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const response = await fetch('http://localhost:4000/')
+      const result = await response.json()
+      setData(result)
+      setLoading(false)
+
+    }
+
+    fetchData()
+  },[])
+  return (
+            
+            <div className="bg-slate-50 text-black rounded-xl flex align-middle justify-center w-80">Hello, from Next server,
+            {loading?<div>Loading...</div>:
+            <div>{data.message}</div>}</div>
   );
 }
