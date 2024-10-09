faefe8d236d0f87af06cef128062e43fd3992ef5
Shahzer Baig <shahzer.baig.sb@gmail.com>
2024-10-09 22:19:40 +0530

    Fix: Bug Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 import { useEffect, useState } from "react";
 
+interface DataType {
+  message: string; // Define the structure of your data
+}
+
 
 export default function Home() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<DataType | null>(null);
   const [loading, setLoading] = useState(true);
-
+   
   useEffect(()=>{
     const fetchData = async () =>{
       const response = await fetch('http://localhost:4000/')
@@ -21,6 +25,6 @@ export default function Home() {
             
             <div className="bg-slate-50 text-black rounded-xl flex align-middle justify-center w-80">Hello, from Next server,
             {loading?<div>Loading...</div>:
-            <div>{data.message}</div>}</div>
+            <div>{data?.message}</div>}</div>
   );
 }
