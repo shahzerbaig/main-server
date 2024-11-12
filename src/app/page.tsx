bb84388d2661d4cfd69fafa916b7ccc778a42c4b
Shahzer Baig <shahzer.baig.sb@gmail.com>
2024-11-12 12:45:01 +0530

    Corrected Eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import HelloCard from "@/components/HelloCard";
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function root(){
-
-  return  (
+export default function root() {
+  return (
     <div className="flex">
-  <Link href="/sign-in">  
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-transform transform active:scale-95">
-    Go to Sign In
-  </button>
-  </Link>
-  
-</div>
-  )
-
+      <Link href="/sign-in">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-transform transform active:scale-95">
+          Go to Sign In
+        </button>
+      </Link>
+    </div>
+  );
 }
