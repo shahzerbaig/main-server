3bd8e3d47e73f3955a75e65a46a04a49942875c1
Shahzer Baig <shahzer.baig.sb@gmail.com>
2024-11-12 11:27:29 +0530

    1. Meta Tag added \n 2. Sign In Link Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,17 @@
-'use client'
-import { useEffect, useState } from "react";
+import HelloCard from "@/components/HelloCard";
+import Link from 'next/link';
 
-interface DataType {
-  message: string; // Define the structure of your data
-}
+export default function root(){
 
-
-export default function Home() {
-  const [data, setData] = useState<DataType | null>(null);
-  const [loading, setLoading] = useState(true);
+  return  (
+    <div className="flex">
+  <Link href="/sign-in">  
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-transform transform active:scale-95">
+    Go to Sign In
+  </button>
+  </Link>
   
-  const domain = "https://159.89.173.118/api/" 
-  useEffect(()=>{
-    const fetchData = async () =>{
-      const response = await fetch(domain)
-      const result = await response.json()
-      setData(result)
-      setLoading(false)
+</div>
+  )
 
-    }
-
-    fetchData()
-  },[])
-  return (
-            
-            <div className="bg-slate-50 text-black rounded-xl flex align-middle justify-center w-80">Hello, from Next server,
-            {loading?<div>Loading...</div>:
-            <div>{data?.message}</div>}</div>
-  );
 }
